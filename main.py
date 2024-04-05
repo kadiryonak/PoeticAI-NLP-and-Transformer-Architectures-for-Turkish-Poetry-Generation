e48b1eb0fc71f650cd3efde2e744e4ef7562a059
Kadir Yönak <128465120+kadiryonak@users.noreply.github.com>
2024-04-05 14:48:45 +0300

    main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ def main():
     df = load_and_preprocess_data()
     model, tokenizer = load_model_and_tokenizer()
     PoemDataset(prepared_data)
-    
-     """ This part will be inherited from the training.py page.  """
-    
+    """ 
+     This section will be inherited from the training.py page
+     and the training.py page will be arranged in accordance with main.py.  
+    """
     train_model(df, tokenizer)
     instruction = "Güneşin batışı"
     generate_poems(instruction)
