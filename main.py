0c3ee802597ea9c7c0558045275b60999eb37fb4
Kadir Yönak <128465120+kadiryonak@users.noreply.github.com>
2024-04-05 14:37:12 +0300

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from inference import generate_poems
 def main():
     df = load_and_preprocess_data()
     model, tokenizer = load_model_and_tokenizer()
-    train_model(df, tokenizer)
+    PoemDataset(prepared_data)
+    
+     """ This part will be inherited from the training.py page.  """
+    
+train_model(df, tokenizer)
     instruction = "Güneşin batışı"
     generate_poems(instruction)
 
