69a7ac4daf6ba1003e3182ea81cde4d5da3f4903
Kadir Yönak <128465120+kadiryonak@users.noreply.github.com>
2024-04-05 11:58:41 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from data_processing import load_and_preprocess_data
+from model import load_model_and_tokenizer
+from training import train_model, PoemDataset
+from inference import generate_poems
+
+def main():
+    df = load_and_preprocess_data()
+    model, tokenizer = load_model_and_tokenizer()
+    train_model(df, tokenizer)
+    instruction = "Güneşin batışı"
+    generate_poems(instruction)
+
+if __name__ == "__main__":
+    main()
